cf3d82680515708ac2a03c44655442f04c465f06
Nicolly <nicollymeireles32@gmail.com>
2023-03-03 22:40:58 -0300

    FrutasPreferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Frutas Preferidas ---");
+Console.WriteLine("1. Melancia");
+Console.WriteLine("2. Maça");
+Console.WriteLine("3. Laranja");
+Console.WriteLine("4. Uva");
+Console.WriteLine("5. Moorango");
+
